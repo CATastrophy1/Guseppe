c9cf00d163e38e0218d943b1e7a02113e1671086
Beata <beata.bartczak34@gmail.com>
2022-12-13 11:57:25 +0100

    worked on reviews

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,27 @@
+//NAV
 const burgerBtn = document.querySelector('.nav__bar-burger-btn')
 const navBox = document.querySelector('.nav__link-box')
 const navLink = document.querySelectorAll('.nav__link-box-link')
 const nav = document.querySelector('.nav')
 
+//50faces
 const facesImgs = document.querySelectorAll('.faces50__img-box-img')
 let numArr = []
-
+//WHEEL
 const wheel = document.querySelector('.tribute__box-wheel')
 const spinBtn = document.querySelector('.tribute__box-spin-btn')
 const finalValue = document.querySelector('.tribute__final-value')
 const tributeAudio = document.querySelector('.tribute__audio')
 
+//OPINIONS 
+const cards = document.querySelectorAll('.guseppism__cards-card')
+const btns = document.querySelectorAll('.guseppism__cards-btn')
+const btn1 = document.querySelector('.guseppism__cards-btn--1')
+const btn2 = document.querySelector('.guseppism__cards-btn--2')
+const btn3 = document.querySelector('.guseppism__cards-btn--3')
+
+
+//WHEEL
 
 let deg = 1
 let rounds = 5
@@ -36,38 +47,76 @@ const handleInterval = () => {
 
 			let topping
 
-			if(randomDegree > 1 && randomDegree < 45) {
-				topping='z salami'	
+			if (randomDegree > 1 && randomDegree < 45) {
+				topping = 'z salami'
 			} else if (randomDegree > 45 && randomDegree < 90) {
-				topping='z pepperoni'
+				topping = 'z pepperoni'
 			} else if (randomDegree > 90 && randomDegree < 135) {
-				topping='z pieczarkami'
+				topping = 'z pieczarkami'
 			} else if (randomDegree > 135 && randomDegree < 180) {
-				topping='margheritę'
+				topping = 'margheritę'
 			} else if (randomDegree > 180 && randomDegree < 225) {
-				topping='z serami'
+				topping = 'z serami'
 			} else if (randomDegree > 225 && randomDegree < 270) {
-				topping='z ananasem'
+				topping = 'z ananasem'
 			} else if (randomDegree > 270 && randomDegree < 315) {
 				topping = `z brokułem`
 			} else if (randomDegree > 315 && randomDegree < 360) {
 				topping = 'z cebulą'
 			}
-			
-			finalValue.textContent=`Musisz podarować Paniczowi pizzę ${topping}`
+
+			finalValue.textContent = `Musisz podarować Paniczowi pizzę ${topping}`
 		}
 	}, 1)
 }
+//OPINIONS
 
-const handleSlider = () => {
+
+
+const slideRight = () => {
+	if(cards[0].classList.contains('active-card')) {
+		cards[0].classList.remove('active-card')
+		cards[1].classList.add('active-card')
+	} else if (cards[1].classList.contains('active-card')) {
+		cards[1].classList.remove('active-card')
+		cards[2].classList.add('active-card')
+	} else {
+		cards[2].classList.remove('active-card')
+		cards[0].classList.add('active-card')
+	}
+}
+const intervalFunction = () => {
 	
+	const sliderInterval = setInterval(slideRight,5000)
 }
 
+const handleDotsBtns = (e) => {
+	btns.forEach(btn => btn.style.background='#ddd')
+	e.target.style.background='#000'
+	cards.forEach(card => {
+				card.classList.remove('active-card')
+			})
+	if(e.target===btn1) {	cards[0].classList.add('active-card')
 
+	} else if (e.target===btn2) {cards[1].classList.add('active-card')
+
+	} else if (e.target===btn3) {
+		cards[2].classList.add('active-card')
+	}
+}
+
+// const handleDots = (e) => {
+// 	cards.forEach(card => {
+// 		card.classList.remove('active-card')
+// 	})
+// 	e.target.style.backgroundColor = '#000'
+// }
+
+//50FACES
 function generateUniqueRandom(maxNr) {
 	//Generate random number
 	let random = Math.floor(Math.random() * maxNr)
-	
+
 	//Coerce to number by boxing
 	random = Number(random)
 	if (!numArr.includes(random)) {
@@ -84,21 +133,17 @@ function generateUniqueRandom(maxNr) {
 	}
 }
 
+facesImgs.forEach(img => {
+	const random = generateUniqueRandom(20)
+
+	img.style.backgroundImage = `url('dist/img/${random}.jpg')`
+})
 
 
-// const randomImg = () => {
-	
-	// }
-	
-	facesImgs.forEach(img => {
-		const random = generateUniqueRandom(20)
-		
-		img.style.backgroundImage = `url('dist/img/${random}.jpg')`
-	})
-	
-	const openNav = () => {
-		window.scrollTo(0, 0)
-		navBox.classList.toggle('active-link-box')
+//NAV
+const openNav = () => {
+	window.scrollTo(0, 0)
+	navBox.classList.toggle('active-link-box')
 	nav.classList.toggle('active-nav')
 }
 
@@ -106,9 +151,26 @@ burgerBtn.addEventListener('click', openNav)
 navLink.forEach(link =>
 	link.addEventListener('click', () => {
 		nav.classList.remove('active-nav')
-		
+
 		navBox.classList.remove('active-link-box')
 	})
-	)
+)
+//WHEEL
+spinBtn.addEventListener('click', handleInterval)
+//OPINIONS
+// btn1.addEventListener('click', () => {
+// 	handleDots()
+// 	cards[0].classList.add('active-card')
+// })
+// btn2.addEventListener('click', () => {
+// 	handleDots()
+// 	cards[1].classList.add('active-card')
 	
-	spinBtn.addEventListener('click', handleInterval)
+// })
+// btn3.addEventListener('click', () => {
+// 	handleDots()
+// 	cards[2].classList.add('active-card')
+// })
+// intervalFunction()
+
+btns.forEach(btn => btn.addEventListener('click', handleDotsBtns))
